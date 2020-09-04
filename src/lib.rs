55a4f33b78451dd8c361276d2654cc230c6f5e02
TangliziGit <tanglizimail@foxmail.com>
2020-09-04 20:30:06 +0800

    completed part 5, write doc and test it

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 use std::collections::HashMap;
 
+/// Used to store a string key to a string value.
+///
+/// # Example
+///
+/// ```
+/// # use kvs::KvStore;
+/// let mut kvs = KvStore::new();
+///
+/// kvs.set("key".to_string(), "value".to_string());
+///
+/// let val = kvs.get("key".to_string());
+/// assert_eq!(val, Some("value".to_string()));
+///
+/// kvs.remove("key".to_string());
+/// let val = kvs.get("key".to_string());
+/// assert_eq!(val, None);
+/// ```
 pub struct KvStore {
     map: HashMap<String, String>,
 }
@@ -14,17 +31,47 @@ impl KvStore {
     }
 
     /// Set the value of a string key to a string
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    /// kvs.set("key".to_string(), "value".to_string());
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.map.insert(key, value);
     }
 
     /// Get the string value of the a string key. If the key does not exist, return None.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use kvs::KvStore;
+    /// let kvs = KvStore::new();
+    /// let value = kvs.get("key".to_string());
+    ///
+    /// assert_eq!(value, None);
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
         self.map.get(&key)
             .map(Clone::clone)
     }
 
     /// Remove a given key.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    /// kvs.set("key".to_string(), "value".to_string());
+    /// kvs.remove("key".to_string());
+    ///
+    /// let value = kvs.get("key".to_string());
+    /// assert_eq!(value, None);
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.map.remove(&key);
     }
